1f954103ec5771a207a1e8cfa898274b11e6edff
webcreation-dev <adjilan2403@gmail.com>
2025-05-14 11:43:41 +0100

    first commit

--- node_modules/@browserbasehq/sdk/version.mjs
+++
@@ -1,2 +0,0 @@
-export const VERSION = '2.5.0'; // x-release-please-version
-//# sourceMappingURL=version.mjs.map
--- node_modules/@langchain/ollama/dist/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./chat_models.js";
-export * from "./embeddings.js";
-export * from "./types.js";
-export * from "./llms.js";
